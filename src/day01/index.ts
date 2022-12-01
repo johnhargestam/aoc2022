8d7b8ab03b19aa8a34e04461fb2d48c037a6383e
= <john.hargestam@gmail.com>
2022-12-01 20:36:35 +0100

    add tests

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,7 +1,10 @@
 import run from 'aocrunner';
 
 const parseInput = (rawInput: string) =>
-  rawInput.split(/\n\n/).map((list) => list.split(/\n/).map((n) => +n));
+  rawInput
+    .trim()
+    .split(/\n\n/)
+    .map((list) => list.split(/\n/).map((n) => +n));
 
 const part1 = (rawInput: string) => {
   const inventories = parseInput(rawInput);
@@ -31,19 +34,47 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`,
+        expected: 24000,
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`,
+        expected: 45000,
+      },
     ],
     solution: part2,
   },
