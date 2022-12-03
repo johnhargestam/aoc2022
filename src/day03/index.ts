9b574387fbb8042fa629a783ac3584fd0fb54aef
= <john.hargestam@gmail.com>
2022-12-03 13:52:02 +0100

    share common function

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -2,37 +2,41 @@ import run from 'aocrunner';
 
 const parseInput = (rawInput: string) => rawInput.trim().split(/\n\s*/);
 
-//char codes: A: 65, Z: 90, a: 97, z: 122
+const commonCharacter = ([first, ...rest]: string[]) =>
+  first
+    .split('')
+    .filter((c) => rest.every((text) => text.includes(c)))
+    .at(0);
+
+//char codes: a-z: 97-122, A-Z: 65-90
 //priorities: a-z: 1-26, A-Z: 27-52
-const priority = (c: string) =>
+const characterPriority = (c: string) =>
   ((n: number) => (n < 97 ? n - 65 + 27 : n - 97 + 1))(c.charCodeAt(0));
 
 const part1 = (rawInput: string) =>
   parseInput(rawInput)
-    .map((rucksack) => [
-      rucksack.slice(0, rucksack.length / 2).split(''),
-      rucksack.slice(rucksack.length / 2).split(''),
+    .map((line) => [
+      line.slice(0, line.length / 2),
+      line.slice(line.length / 2),
     ])
-    .map(([c1, c2]) => c1.filter((c) => c2.includes(c)))
-    .map(([common]) => priority(common))
+    .map(commonCharacter)
+    .filter((c): c is string => !!c)
+    .map(characterPriority)
     .reduce((sum, p) => sum + p);
 
 const part2 = (rawInput: string) =>
   parseInput(rawInput)
     .reduce(
-      (groups: string[][], single, i) =>
+      (groups: string[][], line, i) =>
         ((gi: number) => [
           ...groups.slice(0, gi),
-          (groups[gi] || []).concat(single),
+          (groups[gi] || []).concat(line),
         ])(Math.floor(i / 3)),
       [],
     )
-    .map(([first, ...rest]) =>
-      first
-        .split('')
-        .filter((c) => rest.every((rucksack) => rucksack.includes(c))),
-    )
-    .map(([common]) => priority(common))
+    .map(commonCharacter)
+    .filter((c): c is string => !!c)
+    .map(characterPriority)
     .reduce((sum, p) => sum + p);
 
 run({
