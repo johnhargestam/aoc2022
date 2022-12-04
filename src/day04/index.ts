e6acbfeacfb2053676aa1985b55bd26efbbf74bc
= <john.hargestam@gmail.com>
2022-12-04 11:25:00 +0100

    day04 part2 done

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -10,17 +10,17 @@ const parseInput = (rawInput: string) =>
       pair.split(/,/).map((range) => range.split(/-/).map((n) => +n)),
     ) as Pair[];
 
-const overlaps = ([[a, b], [x, y]]: Pair) =>
+const allOverlap = ([[a, b], [x, y]]: Pair) =>
   (a <= x && b >= y) || (x <= a && y >= b);
 
 const part1 = (rawInput: string) =>
-  parseInput(rawInput).filter(overlaps).length;
+  parseInput(rawInput).filter(allOverlap).length;
 
-const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
+const anyOverlap = ([[a, b], [x, y]]: Pair) =>
+  (b >= x && b <= y) || (y >= a && y <= b);
 
-  return;
-};
+const part2 = (rawInput: string) =>
+  parseInput(rawInput).filter(anyOverlap).length;
 
 run({
   part1: {
@@ -39,7 +39,18 @@ run({
     solution: part1,
   },
   part2: {
-    tests: [],
+    tests: [
+      {
+        input: `
+          2-4,6-8
+          2-3,4-5
+          5-7,7-9
+          2-8,3-7
+          6-6,4-6
+          2-6,4-8`,
+        expected: 4,
+      },
+    ],
     solution: part2,
   },
   trimTestInputs: true,
