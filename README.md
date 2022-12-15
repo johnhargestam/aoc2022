4c074b3b644ba428ee097e52b8f753183f0d8fc5
= <john.hargestam@gmail.com>
2022-12-15 21:56:04 +0100

    day 12 breadth-first

--- README.md
+++ README.md
@@ -22,7 +22,7 @@
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%86/yellow)](src/day11)
-![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
@@ -139,16 +139,16 @@ Both parts: 0.798ms
 
 ```
 Day 11
-Time part 1: 2.236ms
+Time part 1: 2.223ms
 Time part 2: -
-Both parts: 2.2359ms
+Both parts: 2.223ms
 ```
 
 ```
 Day 12
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1611.46ms
+Time part 2: 1629.596ms
+Both parts: 3241.0568ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 18/50
-Total time: 280.416ms
+Total stars: 20/50
+Total time: 3521.459ms
 ```
 
 <!--/RESULTS-->
--- src/day12/index.ts
+++ src/day12/index.ts
@@ -5,76 +5,135 @@ interface Position {
   y: number;
 }
 
-const parseInput = (rawInput: string): string[][] =>
-  rawInput.split(/\n/).map((line) => line.split(''));
+interface Visited {
+  hasPosition: (position: Position) => boolean;
+  addVisit: (position: Position) => Visited;
+}
+
+interface Progress {
+  first: [Position, number];
+  enqueue: (position: Position, steps: number) => Progress;
+  dequeue: () => Progress;
+}
+
+const createHasPosition =
+  (current: Set<number>, xMax: number) =>
+  ({ x, y }: Position): boolean =>
+    current.has(x + y * xMax);
+
+const createAddVisit =
+  (previous: Set<number>, xMax: number) =>
+  ({ x, y }: Position): Visited =>
+    createVisited( new Set([...previous, x + y * xMax]), xMax);
+
+const createVisited = (set: Set<number>, xMax: number): Visited => ({
+  hasPosition: createHasPosition(set, xMax),
+  addVisit: createAddVisit(set, xMax),
+});
+
+const toHeight = (value: string): number =>
+  value === 'S' ? 97 : value === 'E' ? 122 : value.charCodeAt(0);
+
+const createGetPositions = (input: string, xMax: number) => {
+  const values = input.split('').filter((char) => char !== '\n');
+  return (value: string): Position[] =>
+    values
+      .map((char, index) => ({ char, index }))
+      .filter(({ char }) => char === value)
+      .map(({ index }) => ({ x: index % xMax, y: Math.floor(index / xMax) }));
+};
+
+const createEnqueue =
+  (positionSteps: [Position, number][]) =>
+  (position: Position, steps: number): Progress =>
+    createProgress([...positionSteps, [position, steps]]);
+
+const createDequeue =
+  ([, ...rest]: [Position, number][]) =>
+  (): Progress =>
+    createProgress(rest);
+
+const createProgress = (positionSteps: [Position, number][]): Progress => ({
+  first: positionSteps.at(0)!,
+  enqueue: createEnqueue(positionSteps),
+  dequeue: createDequeue(positionSteps),
+});
+
+const parseInput = (rawInput: string) => {
+  const xMax = rawInput.indexOf('\n');
+  const getPositions = createGetPositions(rawInput, xMax);
+  const matrix = rawInput.split('\n').map((row) => row.split('').map(toHeight));
+  return {
+    matrix,
+    start: getPositions('S').at(0)!,
+    end: getPositions('E').at(0)!,
+    lowest: getPositions('a'),
+    visited: createVisited(new Set(), xMax),
+  };
+};
 
 const isValidPosition =
-  (grid: string[][]) =>
+  (matrix: number[][]) =>
   ({ x, y }: Position) =>
-    grid[y] !== undefined && grid[y][x] !== undefined;
-
-const toString = ({ x, y }: Position): string => [x, y].join();
-
-const getHeight = (grid: string[][], { x, y }: Position): number =>
-  grid[y][x] === 'S' ? 97 : grid[y][x] === 'E' ? 122 : grid[y][x].charCodeAt(0);
+    matrix[y] !== undefined && matrix[y][x] !== undefined;
 
 const isClimbable =
-  (grid: string[][], current: Position) =>
+  (matrix: number[][], current: Position) =>
   (candidate: Position): boolean =>
-    getHeight(grid, candidate) <= getHeight(grid, current) + 1;
+    matrix[candidate.y][candidate.x] <= matrix[current.y][current.x] + 1;
 
-const isNotVisited =
-  (visited: Set<string>) =>
-  (position: Position): boolean =>
-    !visited.has(toString(position));
-
-const addPosition = (set: Set<string>, position: Position): Set<string> =>
-  new Set(set.add(toString(position)));
+const equals = (current: Position, target: Position): boolean =>
+  current.x === target.x && current.y === target.y;
 
 const fewestStepsToEnd = (
-  grid: string[][],
-  { x, y }: Position,
-  visited: Set<string>,
-  steps = 0,
-): number =>
-  grid[y][x] === 'E'
-    ? steps
-    : Math.min(
-        ...[
-          { x, y: y + 1 },
-          { x, y: y - 1 },
-          { x: x + 1, y },
-          { x: x - 1, y },
-        ]
-          .filter(isValidPosition(grid))
-          .filter(isClimbable(grid, { x, y }))
-          .filter(isNotVisited(visited))
-          .map((position) =>
-            fewestStepsToEnd(
-              grid,
-              position,
-              addPosition(visited, position),
-              steps + 1,
-            ),
-          ),
-      );
+  matrix: number[][],
+  progress: Progress,
+  visited: Visited,
+  end: Position,
+): number => {
+  const [current, steps] = progress.first;
+  if (equals(current, end)) {
+    return steps;
+  }
+  const [newProgress, newVisited] = [
+    { x: current.x + 1, y: current.y },
+    { x: current.x - 1, y: current.y },
+    { x: current.x, y: current.y + 1 },
+    { x: current.x, y: current.y - 1 },
+  ]
+    .filter(isValidPosition(matrix))
+    .filter(isClimbable(matrix, current))
+    .reduce(
+      ([progress, visited], next) =>
+        !visited.hasPosition(next)
+          ? [
+              progress.enqueue(next, steps + 1),
+              visited.addVisit(next),
+            ]
+          : [progress, visited],
+      [progress, visited],
+    );
 
-const getPosition = (grid: string[][], value: string): Position => {
-  const y = grid.findIndex((row) => row.includes(value));
-  const x = grid[y].indexOf(value);
-  return { x, y };
+  return fewestStepsToEnd(
+    matrix,
+    newProgress.dequeue(),
+    newVisited.addVisit(current),
+    end,
+  );
 };
 
 const part1 = (rawInput: string) => {
-  const grid = parseInput(rawInput);
-  const start = getPosition(grid, 'S');
-  return fewestStepsToEnd(grid, start, addPosition(new Set(), start));
+  const { matrix, start, end, visited } = parseInput(rawInput);
+  const progress = createProgress([[start, 0]]);
+  return fewestStepsToEnd(matrix, progress, visited, end);
 };
 
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput);
-
-  return;
+  const { matrix, start, lowest, end, visited } = parseInput(rawInput);
+  const progress = createProgress(
+    lowest.concat(start).map((position) => [position, 0]),
+  );
+  return fewestStepsToEnd(matrix, progress, visited, end);
 };
 
 run({
@@ -93,9 +152,19 @@ run({
     solution: part1,
   },
   part2: {
-    tests: [],
+    tests: [
+      {
+        input: `
+          Sabqponm
+          abcryxxl
+          accszExk
+          acctuvwj
+          abdefghi`,
+        expected: 29,
+      },
+    ],
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: true,
+  onlyTests: false,
 });
