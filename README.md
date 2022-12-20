85b7f57f3e55be86f956e1dcfa9971cb82aeda20
= <john.hargestam@gmail.com>
2022-12-20 23:57:13 +0100

    day 7 p1 immutable

--- README.md
+++ README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%86/yellow)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
@@ -111,9 +111,9 @@ Both parts: 2.5890999999999997ms
 
 ```
 Day 07
-Time part 1: -
+Time part 1: 4.794ms
 Time part 2: -
-Both parts: -
+Both parts: 4.7944ms
 ```
 
 ```
@@ -167,9 +167,9 @@ Both parts: 1066.9362ms
 
 ```
 Day 15
-Time part 1: 0.702ms
+Time part 1: 0.705ms
 Time part 2: -
-Both parts: 0.7024ms
+Both parts: 0.7049ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 24/50
-Total time: 2350.538ms
+Total stars: 25/50
+Total time: 2355.335ms
 ```
 
 <!--/RESULTS-->
--- src/day07/index.ts
+++ src/day07/index.ts
@@ -1,104 +1,87 @@
 import run from 'aocrunner';
 
-const parseInput = (rawInput: string) =>
+interface Command {
+  cd: string;
+  size: number;
+}
+
+interface Folder {
+  name: string;
+  parent: string;
+  depth: number;
+  size: number;
+}
+
+interface FolderSize {
+  [name: string]: number;
+}
+
+const parseCommand = ([cd, ...ls]: string[]): Command => ({
+  cd: cd.split(/\s/).at(-1) || '',
+  size: ls
+    .slice(1)
+    .map((line) => line.split(/\s/))
+    .filter(([first]) => first !== 'dir')
+    .reduce((sum, [size]) => sum + +size, 0),
+});
+
+const CD_LS = /(\$ cd[^$]+(?:\$ ls[^$]+)?)/;
+
+const notEmpty = ({ length }: string) => length > 0;
+
+const parseCommands = (rawInput: string) =>
   rawInput
-    .split(/\n/)
-    .map((line: string) => (line.startsWith('$ ') ? line.slice(2) : line))
-    .filter((line) => line != 'ls')
-    .map((line) => line.split(/\s/));
+    .split(CD_LS)
+    .filter(notEmpty)
+    .map((cdls) => cdls.split(/\n/).filter(notEmpty))
+    .map(parseCommand);
 
-interface File {
-  name: string;
-  size: number;
-}
-
-interface Directory {
-  name: string;
-  children: Directory[];
-  files: File[];
-}
-
-interface Location {
-  current: Directory;
-  history: Directory[];
-}
-
-interface DirectorySize {
-  name: string;
-  size: number;
-}
-
-const open = ({ current, history }: Location, name: string): Location => {
-  switch (name) {
-    case '/':
-      return { current, history: [] };
-    case '..':
-      const parent = history.at(-1) || current;
-      return {
-        current: parent,
-        history: history.slice(0, -1),
-      };
-    default:
-      const child = current.children.find((child) => child.name == name);
-      return {
-        current: child || current,
-        history: child ? history.concat(current) : history,
-      };
-  }
-};
-
-const spaces = (length: number) => Array(length).fill(' ').join('');
-
-const render = ({ name, children, files }: Directory, depth = 0): string =>
-  `${spaces(depth)}- ${name} (dir)\n`.concat(
-    files
-      .map(
-        (file) =>
-          `${spaces(depth + 2)}- ${file.name} (file, size=${file.size})`,
-      )
-      .concat(children.map((child) => render(child, depth + 2)))
-      .sort()
-      .join('\n'),
-  );
-
-const listSizes = ({ name, children, files }: Directory): DirectorySize[] => {
-  const childSizes = children.map((child) => listSizes(child)).flat();
-  const childTotal = childSizes.reduce((size, child) => size + child.size, 0);
-  const fileTotal = files.reduce((size, file) => size + file.size, 0);
-
-  return [{ name, size: childTotal + fileTotal }].concat(childSizes);
-};
-
-const part1 = (rawInput: string) => {
-  const root: Directory = {
-    name: '/',
-    children: [],
-    files: [],
-  };
-  const history: Directory[] = [];
-
-  parseInput(rawInput).reduce(
-    (location: Location, [a, b]) => {
-      switch (a) {
-        case 'cd':
-          return open(location, b);
-        case 'dir':
-          location.current.children.push({ name: b, children: [], files: [] });
-          return location;
-        default:
-          location.current.files.push({ name: b, size: +a });
-          return location;
-      }
+const toFolders = (commands: Command[]): Folder[] =>
+  commands.reduce(
+    ({ folders, stack }, { cd, size }) =>
+      cd == '..'
+        ? {
+            folders,
+            stack: stack.slice(0, -1),
+          }
+        : {
+            folders: folders.concat({
+              name: stack.concat(cd).join(),
+              depth: stack.length,
+              parent: stack.join(),
+              size,
+            }),
+            stack: stack.concat(cd),
+          },
+    {
+      folders: [] as Folder[],
+      stack: [] as string[],
     },
-    { current: root, history },
+  ).folders;
+
+const parseInput = (rawInput: string): Folder[] =>
+  toFolders(parseCommands(rawInput));
+
+const byDepthDescending = (a: Folder, b: Folder) => b.depth - a.depth;
+
+const getSizes = (folders: Folder[]) =>
+  Object.values(
+    folders
+      .sort(byDepthDescending)
+      .reduce((folderSize: FolderSize, { name, parent, size }) => {
+        const recursiveSize = size + (folderSize[name] || 0);
+        return {
+          ...folderSize,
+          [name]: recursiveSize,
+          [parent]: (folderSize[parent] || 0) + recursiveSize,
+        };
+      }, {}),
   );
 
-  //return render(root);
-
-  return listSizes(root)
-    .filter(({ size }) => size <= 100000)
-    .reduce((total, { size }) => total + size, 0);
-};
+const part1 = (rawInput: string) =>
+  getSizes(parseInput(rawInput))
+    .filter((size) => size <= 100000)
+    .reduce((sum, size) => sum + size);
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
@@ -141,7 +124,35 @@ run({
   },
 
   part2: {
-    tests: [],
+    tests: [
+      {
+        input: `
+          $ cd /
+          $ ls
+          dir a
+          14848514 b.txt
+          8504156 c.dat
+          dir d
+          $ cd a
+          $ ls
+          dir e
+          29116 f
+          2557 g
+          62596 h.lst
+          $ cd e
+          $ ls
+          584 i
+          $ cd ..
+          $ cd ..
+          $ cd d
+          $ ls
+          4060174 j
+          8033020 d.log
+          5626152 d.ext
+          7214296 k`,
+        expected: 24933642,
+      },
+    ],
     solution: part2,
   },
   trimTestInputs: true,
